50d78c5a36a268ba50e23dce9fc04ea4b0e53f8b
Robin Rijkeboer <tunderofnl@gmail.com>
2014-06-05 17:56:56 +0200

    #1 It's now possible to put your full cache on S3.

--- core/components/s3extended/model/s3extended/s3extended.functions.php
+++ core/components/s3extended/model/s3extended/s3extended.functions.php
@@ -17,30 +17,52 @@ class s3extended_functions {
     }
 
     public function generateFileManagerThumbs($url) {
+        $s3CacheFolder = $this->s3->getOption('s3CacheFolder', $this->s3->properties, '_cache');
+        $properties = $this->s3->getPropertyList();
+        $s3CacheFolderURL = $properties['url']. $s3CacheFolder . "/";
+        $cacheToS3 = $this->s3->getOption('cacheToS3', $this->s3->properties,'Yes');
+
         $imageWidth = $this->s3->ctx->getOption('filemanager_image_width', 400);
         $imageHeight = $this->s3->ctx->getOption('filemanager_image_height', 300);
         $thumbHeight = $this->s3->ctx->getOption('filemanager_thumb_height', 60);
         $thumbWidth = $this->s3->ctx->getOption('filemanager_thumb_width', 80);
         $thumbnailQuality = $this->s3->getOption('thumbnailQuality', $this->s3->properties, 90);
+
         $assetsPath = MODX_ASSETS_PATH . "components/s3extended";
         $pathinfo = pathinfo($url);
         $ext = strtolower($pathinfo['extension']);
         $tempFile = $assetsPath . "/tmp/" . uniqid();
         $cacheUrls = array();
-
         //Thumbnail
         $cacheFileName = md5($url) . $thumbWidth . "x" . $thumbHeight . "." . $ext;
         $cacheName = $assetsPath . "/cache/" . $cacheFileName;
         $cacheUrl = MODX_ASSETS_URL . "components/s3extended/cache/" . $cacheFileName;
-        array_push($cacheUrls, $cacheUrl);
+
         //SmallerImage
         $cacheFileName2 = md5($url) . $imageWidth . "x" . $imageHeight . "." . $ext;
         $cacheName2 = $assetsPath . "/cache/" . $cacheFileName2;
         $cacheUrl2 = MODX_ASSETS_URL . "components/s3extended/cache/" . $cacheFileName2;
-        array_push($cacheUrls, $cacheUrl2);
         $url = str_replace(" ", "%20", $url);
 
+        if ($cacheToS3 == "Yes"
+            && $this->s3->driver->if_object_exists($this->s3->bucket, $s3CacheFolder . "/" . $cacheFileName)
+            && $this->s3->driver->if_object_exists($this->s3->bucket, $s3CacheFolder . "/" . $cacheFileName2)) {
+            array_push($cacheUrls, $s3CacheFolderURL . $cacheFileName);
+            array_push($cacheUrls, $s3CacheFolderURL . $cacheFileName2);
+            return $cacheUrls;
+        }
+
         if (file_exists($cacheName) && file_exists($cacheName2)) {
+            if ($cacheToS3 == "Yes") {
+                if($this->uploadFile("", $s3CacheFolder, $cacheFileName, $cacheName)) {
+                    $cacheUrl = $s3CacheFolderURL . $cacheFileName;
+                    unlink($cacheName);
+                }
+                if($this->uploadFile("", $s3CacheFolder, $cacheFileName2, $cacheName2)) {
+                    $cacheUrl2 = $s3CacheFolderURL . $cacheFileName2;
+                    unlink($cacheName2);
+                }
+            }
             array_push($cacheUrls, $cacheUrl);
             array_push($cacheUrls, $cacheUrl2);
             return $cacheUrls;
@@ -109,10 +131,18 @@ class s3extended_functions {
                     break;
             }
         unlink($tempFile);
-        if ($this->s3->getOption('cacheToS3', $this->s3->properties,'Yes') == "Yes") {
-            $this->uploadFile("", $this->s3->getOption('s3CacheFolder', $this->s3->properties, '_cache'), $cacheFileName, $cacheName);
-            $this->uploadFile("", $this->s3->getOption('s3CacheFolder', $this->s3->properties, '_cache'), $cacheFileName2, $cacheName2);
+        if ($cacheToS3 == "Yes") {
+            if($this->uploadFile("", $s3CacheFolder, $cacheFileName, $cacheName)) {
+                $cacheUrl = $s3CacheFolderURL . $cacheFileName;
+                unlink($cacheName);
+            }
+            if($this->uploadFile("", $s3CacheFolder, $cacheFileName2, $cacheName2)) {
+                $cacheUrl2 = $s3CacheFolderURL . $cacheFileName2;
+                unlink($cacheName2);
+            }
         }
+        array_push($cacheUrls, $cacheUrl);
+        array_push($cacheUrls, $cacheUrl2);
         return $cacheUrls;
     }
 
